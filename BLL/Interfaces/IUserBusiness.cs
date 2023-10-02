0d8ddd93aa4073ed4704dfd1fa686db6c5eacff6
TvNam2610 <namtranhy2610@gmail.com>
2023-10-02 21:43:38 +0700

    Tạo api cho user có authenticate

---
+++ BLL/Interfaces/IUserBusiness.cs
@@ -0,0 +1,23 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IUserBusiness
+    {
+        List<UserModel> GetAll();
+
+        UserModel Login(string username, string password);
+        UserModel GetDataById(string id);
+
+        bool Create(UserModel model);
+
+        bool Update(UserModel model);
+
+        bool Delete(string id);
+    }
+}
